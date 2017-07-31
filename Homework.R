5b41c5f4cee30705e1ae32a90d6074f985508212
kevynemiliano <kevynemiliano@hotmail.com>
2017-07-30 21:05:35 -0500

    4

--- Homework.R
+++ Homework.R
@@ -1,3 +1,9 @@
+#The assignment is caching the inverse of a matrix.
+
+
+## This function creates a special "matrix" object that can cache its inverse.
+## it supports setting matrix, getting matrix, setting inverse and getting
+## inverse
 
 makeCacheMatrix <- function(x = matrix()) {
   ## Initialize the inverse property
@@ -30,7 +36,10 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-
+## This function computes the inverse of the special "matrix" 
+## returned by makeCacheMatrix above. If the inverse has already
+## been calculated (and the matrix has not changed), then the
+## cachesolve will retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
   ## Return a matrix that is the inverse of 'x'
